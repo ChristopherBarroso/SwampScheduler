473bcb35e3dbf0d4a05ce62cada02f0b861f7d5f
ChristopherBarroso <101362339+ChristopherBarroso@users.noreply.github.com>
2024-02-04 15:24:51 -0500

    Fixed bug
    Accidentally removed a line, this commit readded it

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ScheduleBuilder from "@components/ScheduleBuilder.tsx";
 import FirstTimeUserPage from "@components/firstTimeUserPage.tsx"; 
 import "./index.css";
 
@@ -8,6 +9,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
+        <ScheduleBuilder />
             <FirstTimeUserPage /> {}
         </QueryClientProvider>
     </React.StrictMode>,
